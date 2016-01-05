10080b309968671c8d7f558842bf59c5740506bd
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2016-01-05 21:08:02 +0000

    Use html5lib instead of lxml; smaller, actually deploys

--- icw/api/citation.py
+++ icw/api/citation.py
@@ -2,12 +2,12 @@ import asyncio
 import datetime
 import logging
 
+
 import functools
 from io import BytesIO
 
 import aiohttp
-import lxml.etree
-#import opengraph
+import html5lib
 import pytz
 import rdflib
 
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 
 SCHEMA = rdflib.Namespace('http://schema.org/')
 
+
 def first_non_null(func):
     @functools.wraps(func)
     def f(*args, **kwargs):
@@ -25,13 +26,17 @@ def first_non_null(func):
                 return item
     return f
 
+
 @first_non_null
 def get_citation_title(doc, uri, og, graph):
     if uri:
         yield graph.value(uri, SCHEMA.headline)
     yield og.get('og:title')
-    for title in doc.xpath('/html/head/title'):
-        yield (title.text or ''.strip()) or None
+    try:
+        yield (doc.getroot().get('head').get('title').text or '').strip() or None
+    except AttributeError:
+        pass
+
 
 @first_non_null
 def get_citation_description(doc, uri, og, graph):
@@ -39,12 +44,14 @@ def get_citation_description(doc, uri, og, graph):
         yield graph.value(uri, SCHEMA.articleBody)
     yield og.get('og:description')
 
+
 @first_non_null
 def get_citation_published(doc, uri, og, graph):
     if uri:
         published = graph.value(uri, SCHEMA.datePublished)
         if published:
             yield published.toPython()
+
 
 @first_non_null
 def get_citation_publisher(doc, uri, og, graph):
@@ -54,6 +61,7 @@ def get_citation_publisher(doc, uri, og, graph):
             yield graph.value(uri, SCHEMA.name)
     yield og.get('og:site_name')
 
+
 @asyncio.coroutine
 def fetch_citation(session, citation):
     print(citation.url)
@@ -61,18 +69,16 @@ def fetch_citation(session, citation):
     if response.status != 200:
         logger.warning("Couldn't retrieve %s (status %d)", citation.url, response.status)
 
-    body = yield from response.read()
+    body = yield from response.text()
 
- #   og = opengraph.OpenGraph()
- #   og.parser(body)
-
-    doc = lxml.etree.fromstring(body, parser=lxml.etree.HTMLParser())
+    doc = html5lib.parse(body)
     graph = rdflib.Graph()
-    for jsonld in doc.xpath("//script[@type='application/ld+json']"):
-        graph.parse(BytesIO(jsonld.text.encode()), response.url, format='json-ld')
+    for jsonld in doc.iter('script'):
+        if jsonld.attrib.get('type') == 'application/ld+json':
+            graph.parse(BytesIO(jsonld.text.encode()), response.url, format='json-ld')
 
-    og = doc.xpath("//meta[starts-with(@property, 'og:')]")
-    og = {e.attrib['property']: e.attrib['content'] for e in og}
+    og = [n for n in doc.iter('meta') if n.attrib.get('property', '').startswith('og:') and 'content' in n.attrib]
+    og = {n.attrib['property']: n.attrib['content'] for n in og}
 
     uri = graph.value(None, SCHEMA.url, rdflib.URIRef(response.url))
 
@@ -86,6 +92,7 @@ def fetch_citation(session, citation):
     citation.last_crawled = pytz.utc.localize(datetime.datetime.utcnow())
 
     session.add(citation)
+
 
 if __name__ == '__main__':
     from .app import get_app
